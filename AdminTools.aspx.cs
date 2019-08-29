629c1a0fc17f4887dd8084f754433630e6f097ca
Miren <miren.patel@potenzaglobalsolutions.com>
2019-08-29 10:56:09 +0530

    Push drive Thru Project
    Push drive Thru Project

---
+++ AdminTools.aspx.cs
@@ -0,0 +1,49 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdminTools : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            lblTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+    }
+
+    protected void GetTime(object sender, EventArgs e)
+    {
+        lblTime.Text = DateTime.Now.ToString();
+    }
+
+    protected void btnResetTimer_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            using (DriveThruEntities db = new DriveThruEntities())
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(),"load","$('#pre-loader').css('display', 'block');",true);
+                var tblSensorData1 = db.tblSensorData1.ToList();
+                var tblSensorData2 = db.tblSensorData2.ToList();
+                var tblSensorData3 = db.tblSensorData3.ToList();
+
+                db.tblSensorData1.RemoveRange(tblSensorData1);
+                db.tblSensorData2.RemoveRange(tblSensorData2);
+                db.tblSensorData3.RemoveRange(tblSensorData3);
+
+                db.SaveChanges();
+
+                Response.Redirect("DriveThruTimer.aspx");
+            }
+        }
+        catch (Exception ex)
+        {
+            utility.log(ex.ToString());
+        }
+    }
+}
---
+++ WebHook.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebHook.aspx.cs" Inherits="WebHook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3 style="text-align: center">Web Hook Data</h3>
+            <h4>
+                <asp:Label ID="lblNoData" runat="server" Style="margin: auto; display: table; border: 0px solid red;"></asp:Label></h4>
+
+                <%--Guid dataMessageGUID = new Guid(Request.QueryString.Get("dataMessageGUID"));<br />
+                int sensorID = Convert.ToInt32(Request.QueryString.Get("sensorID"));<br />
+                string sensorName = Request.QueryString.Get("sensorName");<br />
+                string messageDate = Request.QueryString.Get("messageDate");<br />
+                int batteryLevel = Convert.ToInt32(Request.QueryString.Get("batteryLevel"));<br />
+                int gatewayID = Convert.ToInt32(Request.QueryString.Get("gatewayID"));<br />
+                int signalStrength = Convert.ToInt32(Request.QueryString.Get("signalStrength"));<br />
+                string rawData = Request.QueryString.Get("rawData");<br />--%>
+        </div>
+    </form>
+</body>
+</html>
